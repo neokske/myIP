37923e13f691756038b7c514fe05037e331003b0
Koen <neokske@gmail.com>
2018-11-30 22:44:11 +0100

    :checkered_flag: windows support added

--- functions/copyToClipboard.js
+++ functions/copyToClipboard.js
@@ -1,12 +1,32 @@
 const child_process = require("child_process");
+const WINDOWS = "win32";
+const MACOS = "darwin";
 
-function pbcopy(data) {
+function copyToWindowsClip(data, resolve, reject) {
+  const proc = child_process.spawn("clip");
+  proc.stdin.write(data, err => reject(err));
+  proc.stdin.end();
+  resolve();
+}
+
+function copyToMacos(data, resolve, reject) {
+  const proc = child_process.spawn("pbcopy");
+  proc.stdin.write(data, err => reject(err));
+  proc.stdin.end();
+  resolve();
+}
+
+function copy(data) {
   return new Promise((resolve, reject) => {
-    const proc = child_process.spawn("pbcopy");
-    proc.stdin.write(data, err => reject(err));
-    proc.stdin.end();
-    resolve();
+    switch (process.platform) {
+      case WINDOWS:
+        copyToWindowsClip(data, resolve, reject);
+        break;
+      case MACOS:
+        copyToMacos(data, resolve, reject);
+        break;
+    }
   });
 }
 
-module.exports = pbcopy;
+module.exports = copy;
